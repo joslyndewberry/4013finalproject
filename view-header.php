b9aa93b60e1b965ff9d76afffbf132abfa461257
joslyndewberry <60158127+joslyndewberry@users.noreply.github.com>
2023-12-01 01:17:39 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -1,52 +1,10 @@
-<!-- <!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title><?=$pageTitle?></title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
-    <link rel="stylesheet" href="headerstyle.css">
-  </head>
-  
-  <body>
-    <div class="container">
-<div class="topnav">
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="trainers.php">Trainers</a>
-        </li>
-           <li class="nav-item">
-          <a class="nav-link" href="pokemon.php">Pokemon</a>
-        </li>
-           <li class="nav-item">
-          <a class="nav-link" href="types.php">Types</a>
-        </li>
-           <li class="nav-item">
-          <a class="nav-link" href="stats.php">Stats</a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
-</div>
--->
 
       <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title>Responsive Navbar | CodingNepal</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?=$pageTitle?></title>
     <link rel="stylesheet" href="headerstyle.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
   </head>
@@ -57,13 +15,13 @@
       <label for="check" class="checkbtn">
         <i class="fas fa-bars"></i>
       </label>
-      <label class="logo">DesignX</label>
+      <label class="logo">Gen I</label>
       <ul>
-        <li><a class="active" href="#">Home</a></li>
-        <li><a href="#">About</a></li>
-        <li><a href="#">Services</a></li>
-        <li><a href="#">Contact</a></li>
-        <li><a href="#">Feedback</a></li>
+        <li><a class="active" href="/">Home</a></li>
+        <li><a href="trainer.php">Trainesr</a></li>
+        <li><a href="pokemon.php">Pokemon</a></li>
+        <li><a href="types.php">Types</a></li>
+        <li><a href="stats.php">Stats</a></li>
       </ul>
     </nav>
 
