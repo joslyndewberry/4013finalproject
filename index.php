a95139436904dd96e4d3925301455dbc94d1b859
joslyndewberry <60158127+joslyndewberry@users.noreply.github.com>
2023-12-02 14:25:57 -0600

    Update index.php

--- index.php
+++ index.php
@@ -5,6 +5,13 @@ include "view-header.php";
 
     <h1>Explore...</h1>
 
+    <a href="trainers.php">
+        <button>Trainers</button>
+    </a>
+    <a href="Pokemon.php">
+        <button>Pokemon</button>
+    </a>
+
 <?php
 include "view-footer.php"
 ?>
