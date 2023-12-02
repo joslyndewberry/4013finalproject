7183ff13863958a49d3cb724dbfd2cd761492aa1
joslyndewberry <60158127+joslyndewberry@users.noreply.github.com>
2023-12-02 14:12:19 -0600

    Update index.php

--- index.php
+++ index.php
@@ -3,12 +3,6 @@ $pageTitle = "Home";
 include "view-header.php";
 ?>
 
-<head>
-    <meta charset="utf-8">
-    <link href="https://fonts.cdnfonts.com/css/spacetype" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="style/home.php">
-</head>
-
     <h1>Explore...</h1>
 
 <?php
