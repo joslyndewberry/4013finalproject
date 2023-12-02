789d8989897062a663a8940c76da0594ec189708
joslyndewberry <60158127+joslyndewberry@users.noreply.github.com>
2023-12-02 14:33:39 -0600

    Update index.php

--- index.php
+++ index.php
@@ -6,10 +6,10 @@ include "view-header.php";
     <h1>Explore...</h1>
 
     <a href="trainers.php">
-        <button name= "trainer">Trainers</button>
+        <button class= "trainer">Trainers</button>
     </a>
     <a href="Pokemon.php">
-        <button name= "pokemon">Pokemon</button>
+        <button class= "pokemon">Pokemon</button>
     </a>
 
 <?php
