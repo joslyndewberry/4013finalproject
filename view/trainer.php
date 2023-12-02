2d09dcbd567e1222ab045bedc1a10e499f81f3ef
joslyndewberry <60158127+joslyndewberry@users.noreply.github.com>
2023-12-02 15:34:47 -0600

    Create trainer.php

---
+++ view/trainer.php
@@ -0,0 +1,25 @@
+<h1>Trainers</h1>
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <tr>
+      <th>Trainer ID</th>
+      <th>Name</th>
+      <th>Hometown</th>
+      </tr>
+    </thead>
+    <tbody>
+<?php
+while ($trainer = $trainers->fetch_assoc()) {
+?>
+  <tr>
+    <td><?php echo $trainer['trainer_id']; ?></td>
+    <td><?php echo $trainer['trainer_name']; ?></td>
+    <td><?php echo $trainer['hometown']; ?></td>
+  </tr>
+<?php 
+}
+      ?>
+    </tbody>
+  </table>
+</div>
