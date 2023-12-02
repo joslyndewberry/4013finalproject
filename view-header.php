4aefdfa285f7dfffb5033420dff2797e4abb21e2
joslyndewberry <60158127+joslyndewberry@users.noreply.github.com>
2023-12-02 11:33:16 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -9,7 +9,7 @@
   </head>
 
   <body>
-        <div class="logo"><a href="https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/International_Pok%C3%A9mon_logo.svg/1200px-International_Pok%C3%A9mon_logo.svg.png"></a>
+        <div class="logo"><a href="#"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/International_Pok%C3%A9mon_logo.svg/1200px-International_Pok%C3%A9mon_logo.svg.png"></a>
         </div>
         <nav class="navbar">
               <ul>
