1d729f50c0fe4786f46e3fe3cfad4d33d2362949
joslyndewberry <60158127+joslyndewberry@users.noreply.github.com>
2023-12-02 14:07:13 -0600

    Update index.php

--- index.php
+++ index.php
@@ -5,6 +5,7 @@ include "view-header.php";
 
 <head>
     <meta charset="utf-8">
+    <link href="https://fonts.cdnfonts.com/css/spacetype" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="style/home.php">
 </head>
 
