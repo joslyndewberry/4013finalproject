b12793f8b39e6c3b3616d0b8711666263124da99
joslyndewberry <60158127+joslyndewberry@users.noreply.github.com>
2023-12-02 12:06:04 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?=$pageTitle?></title>
-    <link rel="stylesheet" href="headerstyle.css">
+    <link rel="stylesheet" href="style/header.css">
   </head>
 
   <body>
