5df2debc3071a9fe63803e2c918a52cf52bd3da3
joslyndewberry <60158127+joslyndewberry@users.noreply.github.com>
2023-12-02 11:30:57 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -9,6 +9,8 @@
   </head>
 
   <body>
+        <div class="logo"><a href="https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/International_Pok%C3%A9mon_logo.svg/1200px-International_Pok%C3%A9mon_logo.svg.png"></a>
+        </div>
         <nav class="navbar">
               <ul>
                     <li>
