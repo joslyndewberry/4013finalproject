b58657f52320342d830019f7410d5c2369d423fb
joslyndewberry <60158127+joslyndewberry@users.noreply.github.com>
2023-12-03 14:47:29 -0600

    Update trainer.php

--- view/trainer.php
+++ view/trainer.php
@@ -7,7 +7,7 @@ while ($trainer = $trainers->fetch_assoc()) {
     <p><?php echo $trainer['trainer_name']; ?><p>
     <p class="title">CEO & Founder, Example</p>
     <p>Trainer ID: <?php echo $trainer['trainer_id']; ?></p>
-    <p>Trainer ID: <?php echo $trainer['hometown']; ?></p>
+    <p>Hometown: <?php echo $trainer['hometown']; ?></p>
     <a href="#"><i class="fa fa-dribbble"></i></a>
     <p><button class="edit">Edit</button></p>
 </div>
