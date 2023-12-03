b6a2a3ac70df16881d054403e6985e36085c1a22
joslyndewberry <60158127+joslyndewberry@users.noreply.github.com>
2023-12-03 15:29:11 -0600

    Update trainer.php

--- view/trainer.php
+++ view/trainer.php
@@ -6,7 +6,6 @@ while ($trainer = $trainers->fetch_assoc()) {
   <div class="card">
     <img class="trainer_image" src="<?php echo $trainer['trainer_url']; ?>" style="width:100%">
     <p><?php echo $trainer['trainer_name']; ?><p>
-    <p class="title">CEO & Founder, Example</p>
     <p>Trainer ID: <?php echo $trainer['trainer_id']; ?></p>
     <p>Hometown: <?php echo $trainer['hometown']; ?></p>
     <a href="#"><i class="fa fa-dribbble"></i></a>
