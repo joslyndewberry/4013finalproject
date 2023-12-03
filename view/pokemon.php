c0acad85f4fdd81cbf93065608b861dced93bfa7
joslyndewberry <60158127+joslyndewberry@users.noreply.github.com>
2023-12-03 17:05:47 -0600

    Create pokemon.php

---
+++ view/pokemon.php
@@ -0,0 +1,26 @@
+<div class="modify">
+  <button class="addtrainer" type="button">Add a Pokemon</button>
+</div>
+
+<div class="container">
+<?php
+while ($pokemon = $pokemon->fetch_assoc()) {
+?>
+  <div class="card">
+    <div class= "row">
+      <img class="trainer_image" src="<?php echo $pokemon['image_url']; ?>" style="width:100%">
+    </div>
+    <div class= "column">
+    <p><?php echo $pokemon['pokemon_name']; ?><p>
+    <p>Pokemon ID: <?php echo $pokemon['pokemon_id']; ?></p>
+    <p>Evolution: <?php echo $pokemon['evolution']; ?></p>
+    <p>Base Total: <?php echo $pokemon['base_total']; ?></p>
+    <p>Type: <?php echo $pokemon['type_name']; ?></p>
+    <a href="#"><i class="fa fa-dribbble"></i></a>
+    <p><button class="edit">Edit</button></p>
+    </div>
+</div>
+<?php 
+}
+      ?>
+</div>
