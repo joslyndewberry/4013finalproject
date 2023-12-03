1d0bed4e37ffdf039a9d0dc0946c9870f8e8d022
joslyndewberry <60158127+joslyndewberry@users.noreply.github.com>
2023-12-03 14:05:25 -0600

    Update trainer.php

--- view/trainer.php
+++ view/trainer.php
@@ -1,24 +1,17 @@
-<div class="table-responsive">
-  <table class="table" style="background-color:#87A1BA">
-    <thead>
-      <tr>
-      <th>Trainer ID</th>
-      <th>Name</th>
-      <th>Hometown</th>
-      </tr>
-    </thead>
-    <tbody>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
 <?php
 while ($trainer = $trainers->fetch_assoc()) {
 ?>
-  <tr>
-    <td><?php echo $trainer['trainer_id']; ?></td>
-    <td><?php echo $trainer['trainer_name']; ?></td>
-    <td><?php echo $trainer['hometown']; ?></td>
-  </tr>
+  <div class="card">
+    <img src="img.jpg" alt="John" style="width:100%">
+    <h1><?php echo $trainer['trainer_name']; ?></h1>
+    <p class="title">CEO & Founder, Example</p>
+    <p>Trainer ID: <?php echo $trainer['trainer_id']; ?></p>
+    <p>Trainer ID: <?php echo $trainer['hometown']; ?></p>
+    <a href="#"><i class="fa fa-dribbble"></i></a>
+    <p><button>Edit</button></p>
+</div>
 <?php 
 }
       ?>
-    </tbody>
-  </table>
-</div>
