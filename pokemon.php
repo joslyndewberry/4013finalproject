f594a84c29492a2e522ba05ca0e78e955ae04df7
joslyndewberry <60158127+joslyndewberry@users.noreply.github.com>
2023-12-03 16:34:28 -0600

    Create pokemon.php

---
+++ pokemon.php
@@ -0,0 +1,10 @@
+<?php
+require_once("util-db.php");
+require_once("model/pokemon.php");
+
+$pageTitle = "Pokemon";
+include "view-header.php";
+$pokemon = selectPokemon();
+include "view/pokemon.php";
+include "view-footer.php"
+?>
