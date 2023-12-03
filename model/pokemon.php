e5f17f52e27087373b9e3c1cfd80dbaaac271642
joslyndewberry <60158127+joslyndewberry@users.noreply.github.com>
2023-12-03 16:57:25 -0600

    Update pokemon.php

--- model/pokemon.php
+++ model/pokemon.php
@@ -1,1 +1,16 @@
+<?php
+function selectPokemon() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT pokemon_id, pokemon_name, pt.type_name, evolution, base_total, image_url FROM `pokemon` p JOIN pokemontype pt ON pt.type_id=p.type_id");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 
+?>
