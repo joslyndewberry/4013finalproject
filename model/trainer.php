d20988194e74d8ce360a0cb1a46f9a1d9e06d98b
joslyndewberry <60158127+joslyndewberry@users.noreply.github.com>
2023-12-03 15:03:27 -0600

    Update trainer.php

--- model/trainer.php
+++ model/trainer.php
@@ -2,7 +2,7 @@
 function selectTrainers() {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT trainer_id, trainer_name, hometown FROM `trainer`");
+        $stmt = $conn->prepare("SELECT trainer_id, trainer_name, hometown, trainer_url FROM `trainer`");
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
