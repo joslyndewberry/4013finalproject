e75a92d89138b707a00e02111974e310ace5164c
joslyndewberry <60158127+joslyndewberry@users.noreply.github.com>
2023-12-03 17:06:07 -0600

    Update pokemon.php

--- model/pokemon.php
+++ model/pokemon.php
@@ -2,7 +2,7 @@
 function selectPokemon() {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT pokemon_id, pokemon_name, pt.type_name, evolution, base_total, image_url FROM `pokemon` p JOIN pokemontype pt ON pt.type_id=p.type_id");
+        $stmt = $conn->prepare("SELECT pokemon_id, pokemon_name, type_name, evolution, base_total, image_url FROM `pokemon` p JOIN pokemontype pt ON pt.type_id=p.type_id");
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
