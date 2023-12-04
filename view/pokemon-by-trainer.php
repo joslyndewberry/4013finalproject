1670904e564c9d3ca491ccd9614b1c6ed8fc0a61
joslyndewberry <60158127+joslyndewberry@users.noreply.github.com>
2023-12-03 21:37:04 -0600

    Update pokemon-by-trainer.php

--- view/pokemon-by-trainer.php
+++ view/pokemon-by-trainer.php
@@ -13,7 +13,6 @@ while ($pokemon = $pokemons->fetch_assoc()) {
     <p>Type: <?php echo $pokemon['type_name']; ?></p>
     <a href="#"><i class="fa fa-dribbble"></i></a>
     <p><button class="edit">Stats</button></p>
-    <p><button class="edit">Edit</button></p>
       <p> <form method="post" action=""><input type="hidden" name="p_id" value="<?php echo $pokemon['pokemon_id']; ?>">
         <input type="hidden" name="actionType" value="Delete">
         <button type="submit" class="edit" onclick="return confirm('Are you sure?');">Delete</button>
