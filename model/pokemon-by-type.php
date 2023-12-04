2b0c9c86b539656e536cc35d18f299b7896437b6
joslyndewberry <60158127+joslyndewberry@users.noreply.github.com>
2023-12-03 19:33:03 -0600

    Update pokemon-by-type.php

--- model/pokemon-by-type.php
+++ model/pokemon-by-type.php
@@ -2,7 +2,7 @@
 function selectPokemonByType($tid) {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT pokemon_id, pokemon_name, evolution, p.type_id, type_name FROM `pokemon` p JOIN pokemontype pt WHERE p.type_id = pt.type_id where p.type_id=?");
+        $stmt = $conn->prepare("SELECT pokemon_id, pokemon_name, evolution, image_url, p.type_id, type_name FROM `pokemon` p JOIN pokemontype pt WHERE p.type_id = pt.type_id where p.type_id=?");
         $stmt->bind_param("i", $tid);
         $stmt->execute();
         $result = $stmt->get_result();
