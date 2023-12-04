e2ff54950a0981f42c067d87310412d3ffd9c790
joslyndewberry <60158127+joslyndewberry@users.noreply.github.com>
2023-12-03 20:40:13 -0600

    Update pokemon-by-trainer.php

--- model/pokemon-by-trainer.php
+++ model/pokemon-by-trainer.php
@@ -2,7 +2,7 @@
 function selectPokemonByTrainer($trid) {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT * FROM `trainer` t JOIN capture c ON t.trainer_id = c.trainer_id JOIN pokemon p ON c.pokemon_id = p.pokemon_id WHERE c.trainer_id = ?");
+        $stmt = $conn->prepare("SELECT * FROM `trainer` t JOIN capture c ON t.trainer_id = c.trainer_id JOIN pokemon p ON c.pokemon_id = p.pokemon_id JOIN pokemontype pt ON p.type_id=pt.type_id WHERE c.trainer_id = ?");
         $stmt->bind_param("i", $trid);
         $stmt->execute();
         $result = $stmt->get_result();
