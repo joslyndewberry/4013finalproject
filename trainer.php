98b5f522dee592a2f56b701295b8d435a5c2afec
joslyndewberry <60158127+joslyndewberry@users.noreply.github.com>
2023-12-03 21:24:36 -0600

    Update trainer.php

--- trainer.php
+++ trainer.php
@@ -9,21 +9,21 @@ $trainers = selectTrainers();
 if (isset($_POST['actionType'])) {
   switch($_POST['actionType']) {
     case "Add":
-      if (insertTrainers($_POST['trainer_name'], $_POST['hometown'], $_POST['trainer_url'])) {
+      if (insertTrainers($_POST['t_name'], $_POST['home'], $_POST['t_url'])) {
         echo '<div class="alert alert-secondary" role="alert">Trainer Added.</div>';
       } else {
         '<div class="alert alert-danger" role="alert">Error</div>"';
       }
       break;
       case "Edit":
-      if (updateTrainers($_POST['trainer_name'],  $_POST['hometown'],  $_POST['trainer_url'], $_POST['trainer_id'])) {
+      if (updateTrainers($_POST['t_name'],  $_POST['home'],  $_POST['t_url'], $_POST['t_id'])) {
         echo '<div class="alert alert-secondary" role="alert">Trainer Edited.</div>';
       } else {
         '<div class="alert alert-danger" role="alert">Error</div>"';
       }
       break;
-        case "Delete":
-      if (deleteTrainers($_POST['trainer_id'])) {
+      case "Delete":
+      if (deleteTrainers($_POST['t_id'])) {
         echo '<div class="alert alert-secondary" role="alert">Trainer Removed.</div>';
       } else {
         '<div class="alert alert-danger" role="alert">Error</div>"';
