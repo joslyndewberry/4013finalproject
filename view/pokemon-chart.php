fd7b346a1f0a52d204a062a63de919e19ab0f139
joslyndewberry <60158127+joslyndewberry@users.noreply.github.com>
2023-12-04 01:14:03 -0600

    Update pokemon-chart.php

--- view/pokemon-chart.php
+++ view/pokemon-chart.php
@@ -54,3 +54,26 @@
       }
     });
   </script>
+
+<div class="container">
+<?php
+while ($pokemon = $pokemons->fetch_assoc()) {
+?>
+  <div class="card">
+    <div class= "row">
+      <img class="trainer_image" src="<?php echo $pokemon['image_url']; ?>" style="width:100%">
+    </div>
+    <div class= "column">
+    <p><?php echo $pokemon['pokemon_name']; ?><p>
+    <p>Pokemon ID: <?php echo $pokemon['pokemon_id']; ?></p>
+    <p>Evolution: <?php echo $pokemon['evolution']; ?></p>
+    <p>Type: <?php echo $pokemon['type_name']; ?></p>
+      <p><a target="_blank" href="pokemon-chart.php?id=<?php echo $pokemon['pokemon_id']; ?>">
+        <button class="edit">Stats</button>
+    </a></p>
+    </div>
+</div>
+<?php 
+}
+      ?>
+</div>
