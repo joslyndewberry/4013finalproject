0f0450b561973f437ba725fc0e3d36e294baba7f
joslyndewberry <60158127+joslyndewberry@users.noreply.github.com>
2023-12-04 04:16:27 -0600

    Update pokemon.php

--- pokemon.php
+++ pokemon.php
@@ -7,27 +7,19 @@ include "view-header.php";
 
 if (isset($_POST['actionType'])) {
   switch($_POST['actionType']) {
-    case "Add":
-      if (insertPokemon($_POST['p_name'], $_POST['p_url'], $_POST['evol'], $_POST['hp'], $_POST['attk'], $_POST['def'], $_POST['s_attk'], $_POST['s_def'], $_POST['spd'], $_POST['total'])) {
-        echo '<div class="alert alert-secondary" role="alert">Pokemon Added.</div>';
-      } else {
-        '<div class="alert alert-danger" role="alert">Error</div>"';
-      }
-      break;
-      case "Edit":
-      if (updatePokemon($_POST['p_name'], $_POST['p_url'], $_POST['evol'], $_POST['hp'], $_POST['attk'], $_POST['def'], $_POST['s_attk'], $_POST['s_def'], $_POST['spd'], $_POST['total'], $_POST['p_id'])) {
-        echo '<div class="alert alert-secondary" role="alert">Pokemon Edited.</div>';
-      } else {
-        '<div class="alert alert-danger" role="alert">Error</div>"';
-      }
-      break;
       case "Delete":
       if (deletePokemon($_POST['p_id'])) {
-        echo '<div class="alert alert-secondary" role="alert">Pokemon Removed.</div>';
+        echo '<div class="alert secondary">
+            <span class="closebtn" onclick="this.parentElement.style.display=\'none\';">&times;</span>
+                Pokemon Removed.
+            </div>';
       } else {
-        '<div class="alert alert-danger" role="alert">Error</div>"';
-      }
-      break;
+      echo '<div class="alert danger">
+        <span class="closebtn" onclick="this.parentElement.style.display=\'none\';">&times;</span>
+          Error
+      </div>';
+    }
+  break;
   }
 }
 
