51938a395fbe9d3f1e3c9b475a3c4d4320442162
joslyndewberry <60158127+joslyndewberry@users.noreply.github.com>
2023-12-03 18:31:57 -0600

    Update types.php

--- types.php
+++ types.php
@@ -1,1 +1,11 @@
 
+<?php
+require_once("util-db.php");
+require_once("model/types.php");
+
+$pageTitle = "Types";
+include "view-header.php";
+$types = selectTypes();
+include "view/types.php";
+include "view-footer.php"
+?>
