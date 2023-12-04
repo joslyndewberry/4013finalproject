4fa845b4d4b3d956d616be392c5e48b9a306e8e6
joslyndewberry <60158127+joslyndewberry@users.noreply.github.com>
2023-12-03 19:17:56 -0600

    Update pokemon-by-type.php

--- pokemon-by-type.php
+++ pokemon-by-type.php
@@ -1,1 +1,10 @@
+<?php
+require_once("util-db.php");
+require_once("model/pokemon-by-type.php");
 
+$pageTitle = "Songs by Artist";
+include "view-header.php";
+$songs = selectPokemonByType($_GET['id']);
+include "view/pokemon-by-type.php";
+include "view-footer.php"
+?>
