95dcc228452f1d3e38647ecf2def4716ad6ad5db
joslyndewberry <60158127+joslyndewberry@users.noreply.github.com>
2023-12-03 22:41:00 -0600

    Update trainers-newform.php

--- view/trainers-newform.php
+++ view/trainers-newform.php
@@ -1,5 +1,5 @@
 <!-- Button trigger modal -->
-<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#newTrainerModal">Add New Trainer
+<button type="button" class="addNewTrainer" data-bs-toggle="modal" data-bs-target="#newTrainerModal">Add New Trainer
 </button>
 
 <!-- Modal -->
@@ -8,7 +8,7 @@
     <div class="modal-content">
       <div class="modal-header">
         <h1 class="modal-title fs-5" id="newTrainerModalLabel">New Trainer</h1>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">Close</button>
       </div>
       <div class="modal-body">
         <form method="post" action="">
