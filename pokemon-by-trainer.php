0003fdefd6b4c985a4419ee0683003064f474800
joslyndewberry <60158127+joslyndewberry@users.noreply.github.com>
2023-12-04 03:50:48 -0600

    Update pokemon-by-trainer.php

--- pokemon-by-trainer.php
+++ pokemon-by-trainer.php
@@ -7,23 +7,32 @@ include "view-header.php";
 
 if (isset($_POST['actionType'])) {
   switch($_POST['actionType']) {
-    case "Add":
-      if (insertPokemon($_POST['t_id'], $_POST['p_id'], $_POST['poke'])) {
-       echo '<div class="alert">
-            <span class="closebtn" onclick="this.parentElement.style.display=\'none\';">&times;</span>
+case "Add":
+    if (insertPokemon($_POST['t_id'], $_POST['p_id'], $_POST['poke'])) {
+          echo '<div class="alert success">
+              <span class="closebtn" onclick="this.parentElement.style.display=\'none\';">&times;</span>
                 Pokemon Caught
-            </div>';
-      } else {
-        '<div class="alert alert-danger" role="alert">Error</div>"';
-      }
+                </div>';
+          } else {
+           echo '<div class="alert danger">
+              <span class="closebtn" onclick="this.parentElement.style.display=\'none\';">&times;</span>
+                  Error
+              </div>';
+            }
       break;
-      case "Delete":
+  case "Delete":
       if (deletePokemonFromTrainer($_POST['c_id'])) {
-        echo '<div class="alert alert-secondary" role="alert">Pokemon Released from Trainer.</div>';
-      } else {
-        '<div class="alert alert-danger" role="alert">Error</div>"';
+          echo '<div class="alert secondary">
+              <span class="closebtn" onclick="this.parentElement.style.display=\'none\';">&times;</span>
+                  Pokemon Released from Trainer.
+              </div>';
+          } else {
+              echo '<div class="alert danger">
+                  <span class="closebtn" onclick="this.parentElement.style.display=\'none\';">&times;</span>
+                     Error
+              </div>';
       }
-      break;
+    break;
   }
 }
 
