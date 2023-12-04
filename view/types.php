358d4827c2f4d70262403588c6f85c430d095b2f
joslyndewberry <60158127+joslyndewberry@users.noreply.github.com>
2023-12-03 19:14:17 -0600

    Update types.php

--- view/types.php
+++ view/types.php
@@ -2,7 +2,7 @@
 <?php
 while ($type = $types->fetch_assoc()) {
 ?>
-    <a target="_blank" class="typesbutton" href="pokemon.php"><?php echo $type['type_name']; ?></a>
+    <a target="_blank" class="typesbutton" href="pokemon-by-type.php?id=<?php echo $pokemon['type_id']; ?>"><?php echo $type['type_name']; ?></a>
 <?php 
 }
       ?>
