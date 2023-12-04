e50e02fcbe5ec5a5147d86d2d43b2d87ca332e39
joslyndewberry <60158127+joslyndewberry@users.noreply.github.com>
2023-12-03 21:14:58 -0600

    Update trainer.php

--- trainer.php
+++ trainer.php
@@ -5,6 +5,33 @@ require_once("model/trainer.php");
 $pageTitle = "Trainers";
 include "view-header.php";
 $trainers = selectTrainers();
+
+if (isset($_POST['actionType'])) {
+  switch($_POST['actionType']) {
+    case "Add":
+      if (insertTrainers($_POST['trainer_name'], $_POST['hometown'], $_POST['trainer_url'])) {
+        echo '<div class="alert alert-secondary" role="alert">Trainer Added.</div>';
+      } else {
+        '<div class="alert alert-danger" role="alert">Error</div>"';
+      }
+      break;
+      case "Edit":
+      if (updateTrainers($_POST['trainer_name'],  $_POST['hometown'],  $_POST['trainer_url'], $_POST['trainer_id'])) {
+        echo '<div class="alert alert-secondary" role="alert">Trainer Edited.</div>';
+      } else {
+        '<div class="alert alert-danger" role="alert">Error</div>"';
+      }
+      break;
+        case "Delete":
+      if (deleteTrainers($_POST['trainer_id'])) {
+        echo '<div class="alert alert-secondary" role="alert">Trainer Removed.</div>';
+      } else {
+        '<div class="alert alert-danger" role="alert">Error</div>"';
+      }
+      break;
+  }
+}
+
 include "view/trainer.php";
 include "view-footer.php"
 ?>
