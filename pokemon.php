95f62282070053877584b4ec03ff760c0313de8d
joslyndewberry <60158127+joslyndewberry@users.noreply.github.com>
2023-12-04 04:40:04 -0600

    Update pokemon.php

--- pokemon.php
+++ pokemon.php
@@ -7,6 +7,19 @@ include "view-header.php";
 
 if (isset($_POST['actionType'])) {
   switch($_POST['actionType']) {
+    case "Add":
+      if (insertPokemon($_POST['t_id'], $_POST['p_name'], $_POST['p_url'], $_POST['evol'], $_POST['hp'], $_POST['atk'], $_POST['def'], $_POST['sp_atk'], $_POST['sp_def'], $_POST['spd'], $_POST['total'])) {
+          echo '<div class="alert success">
+             <span class="closebtn" onclick="this.parentElement.style.display=\'none\';">&times;</span>
+                Trainer Added.
+            </div>';
+       } else {
+          echo '<div class="alert danger">
+            <span class="closebtn" onclick="this.parentElement.style.display=\'none\';">&times;</span>
+                Error
+            </div>';
+            }
+      break;
       case "Delete":
       if (deletePokemon($_POST['p_id'])) {
         echo '<div class="alert secondary">
