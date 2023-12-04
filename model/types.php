3211a2c440e596888379805da3dab1cd956e945c
joslyndewberry <60158127+joslyndewberry@users.noreply.github.com>
2023-12-03 18:33:05 -0600

    Create types.php

---
+++ model/types.php
@@ -0,0 +1,16 @@
+<?php
+function selectTypes() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT pokemon_id, pokemon_name, type_name, evolution, base_total, image_url, p.type_id FROM `pokemon` p JOIN pokemontype pt ON pt.type_id=p.type_id");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+?>
