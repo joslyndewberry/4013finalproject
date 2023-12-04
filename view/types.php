95bdc78966e1af19e9adadf55f169ffade0a48bc
joslyndewberry <60158127+joslyndewberry@users.noreply.github.com>
2023-12-03 18:46:17 -0600

    Update types.php

--- view/types.php
+++ view/types.php
@@ -1,6 +1,6 @@
 <div class="container">
 <?php
-while ($types = $types->fetch_assoc()) {
+while ($type = $types->fetch_assoc()) {
 ?>
     <p><button type="button"><?php echo $types['type_name']; ?></button></p>
 <?php 
