e0cf46c9acaa3eeb7ec2e5a7336c41be7cc9360f
joslyndewberry <60158127+joslyndewberry@users.noreply.github.com>
2023-12-04 00:41:16 -0600

    Update pokemon-chart.php

--- view/pokemon-chart.php
+++ view/pokemon-chart.php
@@ -19,33 +19,47 @@
  <script>
   const ctx = document.getElementById('myChart');
 
-  new Chart(ctx, {
-    type: 'radar',
-    data: {
-      labels: ['HP', 'Attack', 'Defense', 'SP Attack', 'SP Defense', 'Speed'],
-      datasets: [
-        <?php
-        $pokemons->data_seek(0); // Reset the pointer to the beginning of the result set
-        while ($pokemon = $pokemons->fetch_assoc()) {
-          echo "{";
-          echo "label: '" . $pokemon['pokemon_name'] . "',";
-          echo "data: [" . $pokemon['hp'] . ", " . $pokemon['attack'] . ", " . $pokemon['defense'] . ", " . $pokemon['sp_attack'] . ", " . $pokemon['sp_defense'] . ", " . $pokemon['speed'] . "],";
-          echo "},";
-        }
-        ?>
-      ]
-    },
-    options: {
-      scales: {
-        r: {
-          angleLines: {
-            display: false
-          },
-          suggestedMin: 10,
-          suggestedMax: 150
+ <script>
+    const ctx = document.getElementById('myChart');
+
+    new Chart(ctx, {
+      type: 'radar',
+      data: {
+        labels: ['HP', 'Attack', 'Defense', 'SP Attack', 'SP Defense', 'Speed'],
+        datasets: [
+          <?php
+          $pokemons->data_seek(0); // Reset the pointer to the beginning of the result set
+          while ($pokemon = $pokemons->fetch_assoc()) {
+            echo "{";
+            echo "label: '" . $pokemon['pokemon_name'] . "',";
+            echo "data: [" . $pokemon['hp'] . ", " . $pokemon['attack'] . ", " . $pokemon['defense'] . ", " . $pokemon['sp_attack'] . ", " . $pokemon['sp_defense'] . ", " . $pokemon['speed'] . "],";
+            echo "},";
+          }
+          ?>
+        ]
+      },
+      options: {
+        scales: {
+          r: {
+            angleLines: {
+              display: false
+            },
+            suggestedMin: 10,
+            suggestedMax: 150
+          }
+        },
+        plugins: {
+          legend: {
+            labels: {
+              font: {
+                family: 'SpaceType, sans-serif'
+              }
+            }
+          }
         }
       }
-    }
-  });
-</script>
+    });
+  </script>
+</body>
+</html>
 
