d49b3a9d02dcc712e900780c9d2942970a9cdf75
joslyndewberry <60158127+joslyndewberry@users.noreply.github.com>
2023-12-03 21:46:28 -0600

    Update pokemon-by-trainer.php

--- pokemon-by-trainer.php
+++ pokemon-by-trainer.php
@@ -4,6 +4,26 @@ require_once("model/pokemon-by-trainer.php");
 
 $pageTitle = "Pokemon Types";
 include "view-header.php";
+
+if (isset($_POST['actionType'])) {
+  switch($_POST['actionType']) {
+    case "Add":
+      if (insertTrainers($_POST['t_name'], $_POST['home'], $_POST['t_url'])) {
+        echo '<div class="alert alert-secondary" role="alert">Trainer Added.</div>';
+      } else {
+        '<div class="alert alert-danger" role="alert">Error</div>"';
+      }
+      break;
+      case "Delete":
+      if (deletePokemonFromTrainer($_POST['c_id'])) {
+        echo '<div class="alert alert-secondary" role="alert">Pokemon Released from Trainer.</div>';
+      } else {
+        '<div class="alert alert-danger" role="alert">Error</div>"';
+      }
+      break;
+  }
+}
+
 $pokemons = selectPokemonByTrainer($_GET['id']);
 include "view/pokemon-by-trainer.php";
 include "view-footer.php"
