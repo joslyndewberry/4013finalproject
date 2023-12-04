cc3292be1868c942174db8f1dd659117a7a0071f
joslyndewberry <60158127+joslyndewberry@users.noreply.github.com>
2023-12-03 19:41:16 -0600

    Update pokemon-by-type.php

--- pokemon-by-type.php
+++ pokemon-by-type.php
@@ -4,7 +4,7 @@ require_once("model/pokemon-by-type.php");
 
 $pageTitle = "Pokemon Types";
 include "view-header.php";
-$pokemons = selectPokemonByType($_GET['tid']);
+$pokemons = selectPokemonByType($_GET['id']);
 include "view/pokemon-by-type.php";
 include "view-footer.php"
 ?>
