228600140f0904dc2e2532939633336d3a4c71d7
joslyndewberry <60158127+joslyndewberry@users.noreply.github.com>
2023-12-03 19:27:08 -0600

    Update pokemon-by-type.php

--- view/pokemon-by-type.php
+++ view/pokemon-by-type.php
@@ -1,1 +1,23 @@
-
+<div class="container">
+<?php
+while ($pokemon = $pokemons->fetch_assoc()) {
+?>
+  <div class="card">
+    <div class= "row">
+      <img class="trainer_image" src="<?php echo $pokemon['image_url']; ?>" style="width:100%">
+    </div>
+    <div class= "column">
+    <p><?php echo $pokemon['pokemon_name']; ?><p>
+    <p>Pokemon ID: <?php echo $pokemon['pokemon_id']; ?></p>
+    <p>Evolution: <?php echo $pokemon['evolution']; ?></p>
+    <p>Type: <?php echo $pokemon['type_name']; ?></p>
+    <a href="#"><i class="fa fa-dribbble"></i></a>
+    <p><button class="edit">Stats</button></p>
+    <p><button class="edit">Edit</button></p>
+    <p><button class="edit">Delete</button></p>
+    </div>
+</div>
+<?php 
+}
+      ?>
+</div>
