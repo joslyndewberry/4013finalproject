4687c987c1e06da8d3fb514a180a82db90b776f0
joslyndewberry <60158127+joslyndewberry@users.noreply.github.com>
2023-12-03 18:38:35 -0600

    Update types.php

--- view/types.php
+++ view/types.php
@@ -1,1 +1,14 @@
+<div class="container">
+<?php
+while ($types = $types->fetch_assoc()) {
+?>
+  <div class="card">
+    <div class= "column">
+    <p><button class="edit"><?php echo $types['type_name']; ?></button></p>
+    </div>
+</div>
+<?php 
+}
+      ?>
+</div>
 
