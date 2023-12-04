2cca22f0217ef3ae277b2589f45ceb9fc0b8af9f
joslyndewberry <60158127+joslyndewberry@users.noreply.github.com>
2023-12-03 20:36:56 -0600

    Create pokemon-by-trainer.php

---
+++ model/pokemon-by-trainer.php
@@ -0,0 +1,17 @@
+<?php
+function selectPokemonByTrainer($trid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT * FROM `trainer` t JOIN capture c ON t.trainer_id = c.trainer_id JOIN pokemon p ON c.pokemon_id = p.pokemon_id WHERE c.trainer_id = ?");
+        $stmt->bind_param("i", $trid);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+?>
